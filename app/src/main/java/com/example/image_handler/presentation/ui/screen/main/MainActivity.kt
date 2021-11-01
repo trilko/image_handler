0a4143180a6eeb153a152dbe215cfb07706afb46
Nikita <nsatyr@provectus.com>
2021-11-02 00:28:34 +0200

    create response with model

--- app/src/main/java/com/example/image_handler/presentation/ui/screen/main/MainActivity.kt
+++ app/src/main/java/com/example/image_handler/presentation/ui/screen/main/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.image_handler.presentation.ui.screen.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.LiveData
 import com.example.image_handler.R
+import com.example.image_handler.data.network.model.ImageResponse
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "TAGGS"
+    private val mainViewModel: MainViewModel by viewModels()
+    private lateinit var liveData: LiveData<ImageResponse>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        liveData = mainViewModel.liveData
+        mainViewModel.updateData()
+        initObservers()
+    }
+
+    private fun initObservers() {
+        liveData.observe(this, {
+            Log.v(TAG, "hey $it")
+
+        })
     }
 }
